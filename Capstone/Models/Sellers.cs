8551ca2dde559ed561e7ef3cf8175a70c94d57f0
DejaMeshae <daemeshae@yahoo.com>
2019-05-22 09:13:11 -0500

    Removed Users Model, added properties to Sellers Model

---
+++ Capstone/Models/Sellers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Sellers 
+    {
+        [Key]
+        public int SellersId { get; set; }
+
+        [Display(Name = "First Name:")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Last Name:")]
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code:")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Profile Picture:")]
+        public byte[] UserPhoto { get; set; }
+
+        public string Lat { get; set; }
+
+        public string Lng { get; set; }
+
+        public IEnumerable<Items> Items { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
