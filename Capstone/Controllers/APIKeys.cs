0d617368bb0328890a37a29c802c250cc0a5a447
DejaMeshae <daemeshae@yahoo.com>
2019-05-26 18:56:15 -0500

    Added OpenWeatherAPI jQuery calls in index view of items

--- Capstone/Controllers/APIKeys.cs
+++ Capstone/Controllers/APIKeys.cs
@@ -8,6 +8,7 @@ namespace Capstone.Controllers
     public class APIKeys
     {
         public static string GoogleAPIKey = "AIzaSyDkHnmp3h5Tbjus8x_L8w2nWwaIrmjXo5U";
+        public static string WeatherAPIKey = "09f387e04e9b4cddd37e514e35f7fddc";
 
     }
 }
